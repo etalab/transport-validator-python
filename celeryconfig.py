9416eeff071ed41a353e5cae059083812c43692e
l-vincent-l <lara.vincent@gmail.com>
2017-10-27 18:44:41 +0200

    Fix configuration

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,3 +1,2 @@
 CELERY_BROKER = 'amqp://localhost'
-CELERY_RESULT_BACKEND = 'mongodb'
-CELERY_MONGODB_BACKEND_SETTINGS = {'host': 'localhost', 'database': 'celery'}
+CELERY_RESULT_BACKEND = 'mongodb://localhost/celery'
