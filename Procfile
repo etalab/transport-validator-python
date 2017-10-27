21b242f055b331a0ce65957bee8d73133fc4bc61
l-vincent-l <lara.vincent@gmail.com>
2017-10-27 18:58:19 +0200

    Move tasks.py file

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: celery -A tasks worker --loglevel=info
+worker: celery -A transport_validator.tasks worker --loglevel=info
---
+++ transport_validator/tasks.py
@@ -0,0 +1,46 @@
+import requests, tempfile, os, transitfeed
+from io import BytesIO
+from transport_validator.validator import Accumulator
+from celery import Celery, states
+
+app = Celery('tasks')
+for envvar in ['CELERY_BROKER', 'CELERY_RESULT_BACKEND']:
+    if envvar in os.environ:
+        app.config_from_envvar(envvar)
+
+@app.task(bind=True)
+def perform(self, url):
+#Download the file
+    r = requests.get(url)
+    self.update_state(state='DOWNLOADED')
+    if r.status_code != 200:
+        raise Exception("Bad status code {} {}".format(url, r.status_code))
+
+#Save it in a temp file
+    d = tempfile.mkdtemp()
+    filename = os.path.join(d, "gtfs.zip")
+    with open(filename, "wb") as f:
+        f.write(r.content)
+
+#Load it
+    accumulator = Accumulator()
+    problems = transitfeed.ProblemReporter(accumulator)
+    gtfs_factory = transitfeed.GetGtfsFactory()
+    loader = gtfs_factory.Loader(filename, problems=problems,
+                                 extra_validation=False,
+                                 gtfs_factory=gtfs_factory)
+    schedule = loader.Load() #this is a transport schedule
+    self.update_state(state='LOADED')
+#Remove tempfiles
+    os.remove(filename)
+    os.rmdir(d)
+
+#Run validation & return results
+    schedule.Validate(validate_children=False)
+    self.update_state(state=states.SUCCESS)
+    accumulator = problems.GetAccumulator()
+    return {
+        "validations":
+        {key: [value.GetDictToFormat() for value in getattr(accumulator, key)]
+         for key in ["errors", "warnings", "notices"]}
+    }
